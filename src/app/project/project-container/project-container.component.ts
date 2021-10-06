6290b9107690ac1b329dd1a58d7bf3808e59d047
Davide Fiorentino <davide.fiorentino01@gmail.com>
2021-10-06 14:44:54 +0200

    implemented project list and dashboard

---
+++ src/app/project/project-container/project-container.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Project } from '@app/models/Project';
+import { LogService } from '@app/shared/log.service';
+import { ProjectService } from '../project.service';
+
+@Component({
+  selector: 'ngprj-project-container',
+  templateUrl: './project-container.component.html',
+  styleUrls: ['./project-container.component.css']
+})
+export class ProjectContainerComponent implements OnInit {
+  selectedProject!: Project;
+  
+  projects: Project[] = [];
+
+  constructor(private projectService: ProjectService) {
+
+     }
+
+  ngOnInit(): void {
+    this.projects = this.projectService.getAll();
+  }
+
+  selectProject(project: Project) {
+    this.selectedProject = this.projectService.get(project.id);
+  }
+
+  submitProjectForm(project: Project) {
+    this.projectService.addProject(project);
+  }
+}
